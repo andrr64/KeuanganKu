8c69fe89ca57e43e3cdbbf90ad4c460b77961d3b
andrr64 <derzandrr03@gmail.com>
2023-10-19 22:17:06 +0700

    menambahkan warna.dart

---
+++ keuanganku/lib/pages/mainpage/beranda/warna.dart
@@ -0,0 +1,7 @@
+import 'dart:ui';
+
+// ignore: camel_case_types
+class Local_Warna {
+  static const warnaButtonUnselected = Color(0xffF0F2FA);
+  static const warnaTextButtonUnselected = Color(0xff9DA6BF);
+}
