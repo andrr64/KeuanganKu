c3d5aed7e8f832e2f7d0e228abc9123790ee9179
andrr64 <derzandrr03@gmail.com>
2023-10-21 14:09:22 +0700

    (cd_u_001) mengubah icon dan merapihkan kode

--- keuanganku/lib/pages/mainpage/mainpage.dart
+++ keuanganku/lib/pages/mainpage/mainpage.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+import 'package:keuanganku/android_system.dart';
 import 'package:keuanganku/pages/mainpage/beranda/beranda.dart';
 import 'package:keuanganku/pages/mainpage/pengeluaran/pengeluaran.dart';
 import 'package:keuanganku/pages/mainpage/tentang/tentang.dart';
@@ -15,59 +15,55 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
-  static final List<Widget> _screens = [
+  final List<Widget> _screens = [
     const HalamanBeranda(),
     const HalamanPengeluaran(),
     const HalamanWallet(),
     const HalamanTentang(),
   ];
+  int _currentIndex = 0;
 
-  Color? warna = Colors.grey[800];
-
-  static int _currentIndex = 0;
-  static final List<BottomNavigationBarItem> _listButton = [
-    const BottomNavigationBarItem(
-        backgroundColor: Warna.warna_primer,
-        icon: Icon(CupertinoIcons.home),
-        label: "Beranda"),
-    const BottomNavigationBarItem(
-        backgroundColor: Warna.warna_primer,
-        icon: Icon(CupertinoIcons.money_dollar),
-        label: "Pengeluaran"),
-    const BottomNavigationBarItem(
-        backgroundColor: Warna.warna_primer,
-        icon: Icon(Icons.wallet),
-        label: "Wallet"),
-    const BottomNavigationBarItem(
-        backgroundColor: Warna.warna_primer,
-        icon: Icon(CupertinoIcons.info),
-        label: "Tentang"),
-  ];
-
-  void _onButtonChange(int index) {
+  // ignore: non_constant_identifier_names, unused_element
+  void _state_onIndexChanged(int index) {
     setState(() {
-      const SystemUiOverlayStyle(
-        systemNavigationBarColor: Colors.transparent,
-        systemNavigationBarIconBrightness: Brightness.dark,
-      );
+      AndroidSys.setNotificationBarColor();
       _currentIndex = index;
     });
   }
 
   @override
   Widget build(BuildContext context) {
+    // Variable : List tombol bottom navbar
+    List<BottomNavigationBarItem> bottomNavBarItems = [
+      BottomNavigationBarItem(
+        icon: Icon(_currentIndex == 0? Icons.analytics : Icons.analytics_outlined),
+        label: "Analisis"),
+        
+      BottomNavigationBarItem(
+        icon: Icon(_currentIndex == 1? CupertinoIcons.money_dollar_circle_fill : CupertinoIcons.money_dollar_circle),
+        label: "Pengeluaran"),
+
+      BottomNavigationBarItem(
+        icon: Icon(_currentIndex == 2? Icons.account_balance_wallet : Icons.account_balance_wallet_outlined),
+        label: "Wallet"),
+
+      BottomNavigationBarItem(
+        icon: Icon(_currentIndex == 3? Icons.info : Icons.info_outline),
+        label: "Tentang"),
+    ];
+
     return Scaffold(
       body: IndexedStack(
         index: _currentIndex,
         children: _screens,
       ),
       bottomNavigationBar: BottomNavigationBar(
-          backgroundColor: Warna.warna_primer,
-          selectedLabelStyle:
-              const TextStyle(fontFamily: "Quicksand", fontSize: 12),
-          currentIndex: _currentIndex,
-          onTap: (index) => _onButtonChange(index),
-          items: _listButton),
+        selectedItemColor: Warna.warna_primer,
+        unselectedItemColor: Warna.warna_primer_50,
+        currentIndex: _currentIndex,
+        items: bottomNavBarItems,
+        onTap: _state_onIndexChanged,
+      ),
     );
   }
 }
