c9b9d6b0dcae45afc1f378e272cb76363a1058de
andrr64 <derzandrr03@gmail.com>
2023-11-22 22:05:15 +0700

    update warna_aplikasi.dart

--- keuanganku/lib/ui/warna_aplikasi.dart
+++ keuanganku/lib/ui/warna_aplikasi.dart
@@ -1,19 +1,20 @@
 import 'dart:ui';
 
-class Warna {
-  static const Color primaryColor = Color.fromARGB(255, 56, 54, 81);
+class ApplicationColors {
+  static const Color primary = Color.fromARGB(255, 56, 54, 81);
 
-  static const Color secondaryColorOrange = Color.fromARGB(255,255,111,49);
+  static const Color secondaryOrange = Color.fromARGB(255,255,111,49);
+  static const Color secondaryYoungPurple = Color(0xffD3C3FA);
 
-  static Color getColorByPercentage({double? percentage}) {
+  static Color primaryColorWidthPercentage({double? percentage}) {
     if (percentage != null) {
       // Konversi persentase alpha ke nilai antara 0 dan 255
       int alpha = (255 * (percentage)/100).round();
 
       // Tentukan nilai ARGB dengan alpha yang sudah dikonversi
       return Color.fromARGB(
-          alpha, primaryColor.red, primaryColor.green, primaryColor.blue);
+          alpha, primary.red, primary.green, primary.blue);
     }
-    return primaryColor;
+    return primary;
   }
 }
