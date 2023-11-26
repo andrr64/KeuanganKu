2b9cc9441baff34b71be07e29a2cf7ffa8e58ff7
andrr64 <derzandrr03@gmail.com>
2023-11-26 16:06:54 +0700

    add data_pengeluaran helper

---
+++ keuanganku/lib/database/helper/data_pengeluaran.dart
@@ -0,0 +1,84 @@
+import 'package:keuanganku/database/model/data_pengeluaran.dart';
+import 'package:sqflite/sqflite.dart';
+
+class DataPengeluaran {
+  Future createTable(Database db) async {
+    await db.execute(DataPengeluaran().sqlCreateQuery);
+  }
+
+  final Map<String, Map<String, String>> _table = {
+    "id": {
+      "name": "id",
+      "type": "INTEGER",
+      "constraint": "AUTO INCREMENT PRIMARY KEY",
+    },
+    "id_kategori": {
+      "name": "id_kategori",
+      "type": "INTEGER",
+      "constraint": "NOT NULL",
+    },
+    "id_wallet": {
+      "name": "id_wallet",
+      "type": "INTEGER",
+      "constraint": "NOT NULL",
+    },
+    "waktu": {
+      "name": "waktu",
+      "type": "TEXT",
+      "constraint": "NOT NULL",
+    },
+    "judul": {
+      "name": "judul",
+      "type": "TEXT",
+      "constraint": "",
+    },
+    "deskripsi": {
+      "name": "deskripsi",
+      "type": "TEXT",
+      "constraint": "",
+    },
+    "nilai": {
+      "name": "nilai",
+      "type": "REAL",
+      "constraint": "",
+    },
+  };
+
+  final _tableName = "data_pengeluaran";
+  
+  String get sqlCreateQuery {
+    String columns = "";
+    _table.forEach((key, value) {
+      columns += "${value['name']} ${value['type']} ${value['constraint']}, ";
+    });
+
+    // Hapus spasi dan koma pada baris terakhir
+    columns = columns.substring(0, columns.length - 2);
+
+    return """
+      CREATE TABLE IF NOT EXISTS $_tableName (
+        $columns
+      )
+    """;
+  }
+
+  Future<List<ModelDataPengeluaran>> readAll(Database db) async {
+    final List<Map<String, dynamic>> results = await db.query(_tableName);
+    List<ModelDataPengeluaran> data = [];
+    
+    for (final result in results) {
+      data.add(ModelDataPengeluaran.fromMap(result));
+    }
+
+    return data;
+  }
+
+  Future<int> insert(ModelDataPengeluaran data, {required Database db}) async {
+    final exitCode = await db.insert(
+      _tableName, 
+      data.toMap(),
+      conflictAlgorithm: ConflictAlgorithm.replace
+    );
+    return exitCode;
+  }
+}
