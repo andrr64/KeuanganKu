97eb07bc47daff36a2152095407c262d328e869a
andrr64 <derzandrr03@gmail.com>
2023-12-03 00:01:19 +0700

    update ktext_field.dart * mengubah tipe widget dari stateful ke stateless * menambah atribut baru -> keyboardType

--- keuanganku/lib/app/reusable_components/ktextfield/ktext_field.dart
+++ keuanganku/lib/app/reusable_components/ktextfield/ktext_field.dart
@@ -1,32 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:keuanganku/app/app_colors.dart';
 
-class KTextField extends StatefulWidget {
-  const KTextField({super.key, required this.fieldController, required this.fieldName, required this.prefixIconColor, this.icon});
+class KTextField extends StatelessWidget {
+  const KTextField({super.key, required this.fieldController, required this.fieldName, required this.prefixIconColor, this.icon, this.keyboardType});
   final String fieldName;
   final IconData? icon;
   final Color prefixIconColor;
   final TextEditingController fieldController;
-
-  @override
-  State<KTextField> createState() => _KTextFieldState();
-}
-
-class _KTextFieldState extends State<KTextField> {
-
+  final TextInputType? keyboardType;
+  
   @override
   Widget build(BuildContext context) {
     return  
       SingleChildScrollView(
         scrollDirection: Axis.vertical,
         child: TextFormField(
-          controller: widget.fieldController,
+          controller: fieldController,
+          keyboardType: keyboardType,
           decoration: InputDecoration(
-            labelText: widget.fieldName,
+            labelText: fieldName,
             border: const OutlineInputBorder(),
-            prefixIcon: Icon(widget.icon, color: widget.prefixIconColor,),
+            prefixIcon: Icon(icon, color: prefixIconColor,),
             focusedBorder: OutlineInputBorder(
-              borderSide: BorderSide(color: widget.prefixIconColor)
+              borderSide: BorderSide(color: prefixIconColor)
             ),
             labelStyle: const TextStyle(color: ApplicationColors.primary)
           ),
