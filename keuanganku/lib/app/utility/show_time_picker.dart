9710b67c15086bbb989145e6e48574daf5fad56b
andrr64 <derzandrr03@gmail.com>
2023-12-03 11:55:40 +0700

    menambah utility baru: time picker * utility ini berguna untuk menampilkan pemilih waktu

---
+++ keuanganku/lib/app/utility/show_time_picker.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+Future<TimeOfDay> tampilkanTimePicker({required BuildContext context, required TimeOfDay waktu}) async {
+  return (await showTimePicker(context: context, initialTime: waktu))!;
+}
