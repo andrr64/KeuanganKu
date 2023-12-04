836d94afe45ee71f3a5231158962b9e860acba17
andrr64 <derzandrr03@gmail.com>
2023-12-04 21:39:53 +0700

    add time input

--- keuanganku/lib/app/main/pengeluaran/widgets/form_data_pengeluaran/form_data_pengeluaran.dart
+++ keuanganku/lib/app/main/pengeluaran/widgets/form_data_pengeluaran/form_data_pengeluaran.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:keuanganku/app/main/pengeluaran/widgets/form_data_pengeluaran/widgets/field_deskripsi/field_deskripsi.dart';
 import 'package:keuanganku/app/main/pengeluaran/widgets/form_data_pengeluaran/widgets/field_judul/field_judul.dart';
 import 'package:keuanganku/app/main/pengeluaran/widgets/form_data_pengeluaran/widgets/field_jumlah_pengeluaran/field_jumlah_pengeluaran.dart';
+import 'package:keuanganku/app/reusable_components/tombol_tambah/tombol_tambah.dart';
+import 'package:keuanganku/app/reusable_components/date_picker/show_date_picker.dart';
+import 'package:keuanganku/app/reusable_components/time_picker/show_time_picker.dart';
 import 'package:keuanganku/database/helper/data_pengeluaran.dart';
 import 'package:keuanganku/database/model/data_pengeluaran.dart';
 import 'package:keuanganku/app/app_colors.dart';
@@ -20,105 +23,114 @@ class _FormDataPengeluaranState extends State<FormDataPengeluaran> {
   final formKey = GlobalKey<FormState>();
 
   DateTime _pickedDate = DateTime.now();
-  DateTime _firstDate = DateTime(2000); // Datetime tahun 2000 mulainya
-  DateTime _lastDate = DateTime.now(); // Akhir datetime
-
+  TimeOfDay _waktu = TimeOfDay.now();
+  final DateTime _firstDate = DateTime(2000); // Datetime tahun 2000 mulainya
+  final DateTime _lastDate = DateTime.now(); // Akhir datetime
   final TextEditingController _controllerFieldJudul = TextEditingController();
   final TextEditingController _controllerFieldDeskripsi = TextEditingController();
   final TextEditingController _controllerFieldNilai = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    void doTampilkanDatePicker(){
-      showDatePicker(
-        context: context, 
-        initialDate: _pickedDate,
-        firstDate: _firstDate, 
-        lastDate: _lastDate
+    // Events
+    void eventSimpanData(){
+      DateTime waktuPengeluaran = DateTime(_pickedDate.year, _pickedDate.month, _pickedDate.day, _waktu.hour, _waktu.minute);
+      ModelDataPengeluaran dataBaru = ModelDataPengeluaran(
+        -1, 
+        _controllerFieldJudul.text, 
+        _controllerFieldDeskripsi.text, 
+        double.tryParse(_controllerFieldNilai.text) ?? 0, 
+        waktuPengeluaran,
+        1, 
+        1, 
+        0
       );
+      SQLDataPengeluaran().create(dataBaru, db: db.database);
+      HalamanBeranda.state.update!();
+      widget.onSaveCallback();
+      Navigator.pop(context);
     }
 
     Widget dummyPadding() => const SizedBox(height: 15,);
 
     return Scaffold(
-      body: Center(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            SizedBox(height: MediaQuery.sizeOf(context).height * 0.05,),
-            SizedBox(
-              width: MediaQuery.sizeOf(context).width  * 0.9,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  const SizedBox(
-                    child: Row(
-                      children: [
-                        Icon(Icons.add),
-                        SizedBox(width: 10,),
-                        Text("Data Baru",
-                          style: TextStyle(
-                            fontFamily: "QuickSand_Bold",
-                            fontSize: 22,
-                            color: ApplicationColors.primary
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  GestureDetector(
-                    child: Icon(Icons.close, color: ApplicationColors.primaryColorWidthPercentage(percentage: 75),),
-                    onTap: (){
-                      Navigator.pop(context);
-                    },
-                  )
-                ],
-              )
-            ),
-            const SizedBox(height: 15,),
-            Container(
-              alignment: Alignment.topLeft,
-              width: MediaQuery.sizeOf(context).width  * 0.9,
-              child: Form(
-                key: formKey,
-                child: Column(
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(height: MediaQuery.sizeOf(context).height * 0.05,),
+              SizedBox(
+                width: MediaQuery.sizeOf(context).width  * 0.9,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                      FieldJudul(_controllerFieldJudul).getWidget(),
-                      dummyPadding(),
-                      FieldDeskripsi(_controllerFieldDeskripsi).getWidget(),
-                      dummyPadding(),
-                      FieldJumlahIDR(_controllerFieldNilai).getWidget(),
-                      dummyPadding(),
-                    ],
+                    const SizedBox(
+                      child: Row(
+                        children: [
+                          Icon(Icons.add),
+                          SizedBox(width: 10,),
+                          Text("Data Baru",
+                            style: TextStyle(
+                              fontFamily: "QuickSand_Bold",
+                              fontSize: 22,
+                              color: ApplicationColors.primary
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    GestureDetector(
+                      child: Icon(Icons.close, color: ApplicationColors.primaryColorWidthPercentage(percentage: 75),),
+                      onTap: (){
+                        Navigator.pop(context);
+                      },
+                    )
+                  ],
                 )
               ),
-            ),
-            FilledButton(
-              style: FilledButton.styleFrom(
-                backgroundColor: ApplicationColors.primary,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(5)
-                )
-              ),
-              onPressed: (){
-                ModelDataPengeluaran dataBaru = ModelDataPengeluaran(
-                  -1, 
-                  _controllerFieldJudul.text, 
-                  _controllerFieldDeskripsi.text, 
-                  double.tryParse(_controllerFieldNilai.text) ?? 0, 
-                  DateTime.now(),
-                  1, 
-                  1, 
-                  0
-                );
-                SQLDataPengeluaran().create(dataBaru, db: db.database);
-                HalamanBeranda.state.update!();
-                widget.onSaveCallback();
-                Navigator.pop(context);
-              }, 
-              child: const Text("Tambah")
-            )
-          ],
+              const SizedBox(height: 15,),
+              Container(
+                alignment: Alignment.topLeft,
+                width: MediaQuery.sizeOf(context).width  * 0.9,
+                child: Form(
+                  key: formKey,
+                  child: Column(
+                    children: [
+                        FieldJudul(_controllerFieldJudul).getWidget(),
+                        dummyPadding(),
+                        FieldDeskripsi(_controllerFieldDeskripsi).getWidget(),
+                        dummyPadding(),
+                        FieldJumlahIDR(_controllerFieldNilai).getWidget(),
+                        dummyPadding(),
+                        TombolTambah(ketikaDitekan: eventSimpanData).getWidget(),
+                        ElevatedButton(
+                          onPressed: () async {
+                            _pickedDate = await tampilkanDatePicker(
+                              context: context, 
+                              waktuAwal: _firstDate, 
+                              waktuAkhir: _lastDate, 
+                              waktuInisialisasi: _pickedDate
+                            );
+                          }, 
+                          child: const Text("Tanggal")
+                        ),
+                        ElevatedButton(
+                          onPressed: () async {
+                            _waktu = await tampilkanTimePicker(
+                              context: context, 
+                              waktu: _waktu
+                            );
+                          }, 
+                          child: const Text("Waktu"))
+                      
+                      ],
+                  )
+                ),
+              ),        
+            ],
+          ),
         ),
       ),
     );
