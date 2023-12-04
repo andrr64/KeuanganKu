b81679df567fd0732ecacef1dda0c1f42cff5a09
andrr64 <derzandrr03@gmail.com>
2023-12-04 21:36:04 +0700

    change entry mode: inputOnly

--- keuanganku/lib/app/reusable_components/time_picker/show_time_picker.dart
+++ keuanganku/lib/app/reusable_components/time_picker/show_time_picker.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
 
 Future<TimeOfDay> tampilkanTimePicker({required BuildContext context, required TimeOfDay waktu}) async {
-  return (await showTimePicker(context: context, initialTime: waktu))!;
+  return (
+    await showTimePicker(
+      context: context, 
+      initialTime: waktu,
+      initialEntryMode: TimePickerEntryMode.inputOnly
+    )
+  )!;
 }
