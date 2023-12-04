71655ad2301dea429099b9297964938c6027d560
andrr64 <derzandrr03@gmail.com>
2023-12-04 22:15:12 +0700

    Update bar_chart

--- keuanganku/lib/app/main/beranda/widgets/ringkasan_grafik/ringkasan_grafik.dart
+++ keuanganku/lib/app/main/beranda/widgets/ringkasan_grafik/ringkasan_grafik.dart
@@ -1,6 +1,8 @@
 // ignore_for_file: non_constant_identifier_names
 import 'package:flutter/material.dart';
 import 'package:keuanganku/database/helper/data_pemasukan_x_pengeluaran.dart';
+import 'package:keuanganku/database/helper/data_pengeluaran.dart';
+import 'package:keuanganku/database/model/data_pengeluaran.dart';
 import 'package:keuanganku/database/model/data_transaksi.dart';
 import 'package:keuanganku/app/main/wrap.dart';
 import 'package:keuanganku/enum/data_transaksi.dart';
@@ -8,6 +10,8 @@ import 'package:keuanganku/app/main/beranda/widgets/ringkasan_grafik/widgets/gra
 import 'package:keuanganku/app/main/beranda/widgets/ringkasan_grafik/widgets/tab_jenis_transaksi/tab_jenis_transaksi.dart';
 import 'package:keuanganku/app/main/beranda/widgets/ringkasan_grafik/widgets/tab_waktu_transaksi/tab_waktu_transaksi.dart';
 import 'package:keuanganku/app/reusable_components/bar_chart/data.dart';
+import 'package:keuanganku/main.dart';
+import 'package:keuanganku/util/date_searching.dart';
 import 'package:keuanganku/util/get_currency.dart';
 import 'package:keuanganku/util/random_algo.dart';
 import 'package:keuanganku/util/vector_operation.dart';
@@ -17,17 +21,35 @@ class Data {
   int indeksTabJenisTransaksi = 0;
   int indeksTabWaktuTransaksi = 0;
 
-  List<BarChartXY> get _dataBarChartPengeluaran {
+  Future<List<BarChartXY>> get _barChartPemasukan async {
     //TODO: API_GET_Database_dataBarChartPengeluaran(x)
+
+    // 1. Dapatkan range tanggal dari senin ke minggu, misal
+    // Senin (1) - Minggu (7)
+    List<DateTime> tanggalSeninKeMinggu = getRangeTanggalSeninKeMinggu();
+
+    // Mengambil list nilai per-tiap hari
+    // Senin = 23000 + 2500 = 25500
+    // Selasa = ...
+    Future<List<double>> getDataMingguan(DateTime tanggal) async {
+      final List<ModelDataPengeluaran> data = await SQLDataPengeluaran().readWithClause(clause: SQLDataPengeluaran().waktuClauseTanggal(tanggal), db: db.database);
+      final List<double> filteredData = data
+          .where((e) => e.nilai != null) // Filter nilai yang bukan null
+          .map((e) => e.nilai!) // Mengonversi nilai nullable menjadi non-nullable
+          .toList();
+      return filteredData;
+    }
+
     switch (waktuTransaksi) {
       case WaktuTransaksi.MINGGUAN:
         return [
-          BarChartXY(xValue: 0, yValue: generateRandomNumber(10000, 10000000)),
-          BarChartXY(xValue: 1, yValue: generateRandomNumber(10000, 10000000)),
-          BarChartXY(xValue: 2, yValue: generateRandomNumber(10000, 10000000)),
-          BarChartXY(xValue: 4, yValue: generateRandomNumber(10000, 10000000)),
-          BarChartXY(xValue: 5, yValue: generateRandomNumber(10000, 10000000)),
-          BarChartXY(xValue: 6, yValue: generateRandomNumber(10000, 10000000)),
+          BarChartXY(xValue: 0, yValue: sumList(await getDataMingguan(tanggalSeninKeMinggu[0]))),
+          BarChartXY(xValue: 1, yValue: sumList(await getDataMingguan(tanggalSeninKeMinggu[1]))),
+          BarChartXY(xValue: 2, yValue: sumList(await getDataMingguan(tanggalSeninKeMinggu[2]))),
+          BarChartXY(xValue: 3, yValue: sumList(await getDataMingguan(tanggalSeninKeMinggu[3]))),
+          BarChartXY(xValue: 4, yValue: sumList(await getDataMingguan(tanggalSeninKeMinggu[4]))),
+          BarChartXY(xValue: 5, yValue: sumList(await getDataMingguan(tanggalSeninKeMinggu[5]))),
+          BarChartXY(xValue: 6, yValue: sumList(await getDataMingguan(tanggalSeninKeMinggu[6]))),
         ];
       case WaktuTransaksi.TAHUNAN:
         return [
@@ -48,7 +70,7 @@ class Data {
     }
   }
 
-  List<BarChartXY> get _barChartPemasukan {
+  List<BarChartXY> get _barChartPengeluaran {
     //TODO: API_GET_Database_dataBarChartPemasuakn(x)
     switch (waktuTransaksi) {
       case WaktuTransaksi.MINGGUAN:
@@ -56,6 +78,7 @@ class Data {
           BarChartXY(xValue: 0, yValue: generateRandomNumber(10000, 10000000)),
           BarChartXY(xValue: 1, yValue: generateRandomNumber(10000, 10000000)),
           BarChartXY(xValue: 2, yValue: generateRandomNumber(10000, 10000000)),
+          BarChartXY(xValue: 3, yValue: generateRandomNumber(10000, 10000000)),
           BarChartXY(xValue: 4, yValue: generateRandomNumber(10000, 10000000)),
           BarChartXY(xValue: 5, yValue: generateRandomNumber(10000, 10000000)),
           BarChartXY(xValue: 6, yValue: generateRandomNumber(10000, 10000000)),
@@ -79,12 +102,12 @@ class Data {
     }
   }
 
-  List<BarChartXY> get arrayBarChart {
+  Future<List<BarChartXY>> get arrayBarChart async{
     switch (jenisTransaksi) {
       case JenisTransaksi.PEMASUKAN:
-        return _dataBarChartPengeluaran;
+        return await _barChartPemasukan;
       case JenisTransaksi.PENGELUARAN:
-        return _barChartPemasukan;
+        return _barChartPengeluaran;
       default:
         return [];
     }
@@ -109,11 +132,22 @@ class RingkasanGrafik{
     // WIDGETS
     widgetTabJenisTransaksi() => TabJenisTransaksi(indeksTab: RingkasanGrafik.data.indeksTabJenisTransaksi);
     widgetTabWaktuTransaksi() => TabWaktuTransaksi(indeksWaktuTransaksi: RingkasanGrafik.data.indeksTabWaktuTransaksi,).getWidget(context);
-    widgetGrafikBar() => GrafikBar(
-        jenisTransaksi: RingkasanGrafik.data.jenisTransaksi, 
-        waktuTransaksi: RingkasanGrafik.data.waktuTransaksi,
-        dataBarChart: RingkasanGrafik.data.arrayBarChart
-    );
+    widgetGrafikBar() {
+      return FutureBuilder(
+        future: RingkasanGrafik.data.arrayBarChart, 
+        builder: (context, snapshot){
+          if (snapshot.hasData){
+            return GrafikBar(
+                jenisTransaksi: RingkasanGrafik.data.jenisTransaksi, 
+                waktuTransaksi: RingkasanGrafik.data.waktuTransaksi,
+                dataBarChart: snapshot.data!
+            );
+          } else {
+            return const CircularProgressIndicator();
+          }
+        }
+      );
+    }
     
     return Column(
       children: [
