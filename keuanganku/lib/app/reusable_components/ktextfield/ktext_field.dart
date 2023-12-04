f11c9a3754316003f6c4a2ca899c4b574497c8c4
andrr64 <derzandrr03@gmail.com>
2023-12-04 22:57:49 +0700

    add kiconfield

--- keuanganku/lib/app/reusable_components/ktextfield/ktext_field.dart
+++ keuanganku/lib/app/reusable_components/ktextfield/ktext_field.dart
@@ -2,31 +2,39 @@ import 'package:flutter/material.dart';
 import 'package:keuanganku/app/app_colors.dart';
 
 class KTextField extends StatelessWidget {
-  const KTextField({super.key, required this.fieldController, required this.fieldName, required this.prefixIconColor, this.icon, this.keyboardType});
+  const KTextField({
+    super.key,
+    required this.fieldController,
+    required this.fieldName,
+    required this.prefixIconColor,
+    this.icon,
+    this.keyboardType,
+    this.readOnly}
+  );
+
   final String fieldName;
   final IconData? icon;
   final Color prefixIconColor;
   final TextEditingController fieldController;
   final TextInputType? keyboardType;
+  final bool? readOnly;
   
   @override
   Widget build(BuildContext context) {
     return  
-      SingleChildScrollView(
-        scrollDirection: Axis.vertical,
-        child: TextFormField(
-          controller: fieldController,
-          keyboardType: keyboardType,
-          decoration: InputDecoration(
-            labelText: fieldName,
-            border: const OutlineInputBorder(),
-            prefixIcon: Icon(icon, color: prefixIconColor,),
-            focusedBorder: OutlineInputBorder(
-              borderSide: BorderSide(color: prefixIconColor)
-            ),
-            labelStyle: const TextStyle(color: ApplicationColors.primary)
+      TextFormField(
+        controller: fieldController,
+        keyboardType: keyboardType,
+        readOnly: readOnly?? false,
+        decoration: InputDecoration(
+          labelText: fieldName,
+          border: const OutlineInputBorder(),
+          prefixIcon: Icon(icon, color: prefixIconColor,),
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: prefixIconColor)
           ),
+          labelStyle: const TextStyle(color: ApplicationColors.primary)
         ),
-    );
+      );
   }
 }
