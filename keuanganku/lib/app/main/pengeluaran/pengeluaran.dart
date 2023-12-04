af8ce86d24826491f02ed520bd40a4e38aa432e2
andrr64 <derzandrr03@gmail.com>
2023-12-05 01:14:22 +0700

    memulai pengembangan UI pengeluaran

--- keuanganku/lib/app/main/pengeluaran/pengeluaran.dart
+++ keuanganku/lib/app/main/pengeluaran/pengeluaran.dart
@@ -1,21 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:keuanganku/app/reusable_components/card_data_transaksi/card_data_transaksi.dart';
-import 'package:keuanganku/database/helper/data_pengeluaran.dart';
 import 'package:keuanganku/app/app_colors.dart';
 import 'package:keuanganku/app/main/pengeluaran/widgets/form_data_pengeluaran/form_data_pengeluaran.dart';
 import 'package:keuanganku/app/state_bridge.dart';
-import 'package:keuanganku/main.dart';
+
 
 class HalamanPengeluaran extends StatefulWidget {
   const HalamanPengeluaran({super.key});
-
   static StateBridge state = StateBridge();
-
   @override
   State<HalamanPengeluaran> createState() => _HalamanPengeluaranState();
 }
 
 class _HalamanPengeluaranState extends State<HalamanPengeluaran> {
+
   @override
   void initState() {
     super.initState();
@@ -41,6 +38,16 @@ class _HalamanPengeluaranState extends State<HalamanPengeluaran> {
       );
     }
 
+    Size display = MediaQuery.sizeOf(context);
+
+    Widget buildBody(){
+      return Column(
+        children: [
+
+        ],
+      );
+    }
+
     return Scaffold(
       backgroundColor: Colors.white,
       floatingActionButton: FloatingActionButton(
@@ -48,37 +55,14 @@ class _HalamanPengeluaranState extends State<HalamanPengeluaran> {
         onPressed: tambahDataBaru,
         child: const Icon(Icons.add, color: Colors.white,),
       ),
-      body: Center(
-        child: SizedBox(
-          width: MediaQuery.sizeOf(context).width * 0.9,
-          child: FutureBuilder(
-            future: SQLDataPengeluaran().readAll(db.database), 
-            builder: (context, snapshot){
-              if (snapshot.hasData){
-                if (snapshot.data!.isEmpty){
-                  return const Text("Dude, you dont have data");
-                } 
-                else {
-                  return ListView.builder(
-                    itemCount: snapshot.data!.length,
-                    itemBuilder: (context, index) => CardTransaksi(
-                      const Icon(Icons.store), 
-                      onPressed: (){
-                        showAboutDialog(context: context);
-                      },
-                      kategori: snapshot.data![index].judul!,
-                      title: snapshot.data![index].judul!,
-                      waktu: snapshot.data![index].waktu!.toIso8601String(),
-                      jumlah: snapshot.data![index].nilai!,
-                    )
-                  );
-                }
-              } else {
-                return const CircularProgressIndicator(color: ApplicationColors.primary,);
-              }
-            }
-          ),
-        ),
+      body: Container(
+        width: display.width * 0.9,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            buildBody()
+          ],
+        )
       )
     );
   }
