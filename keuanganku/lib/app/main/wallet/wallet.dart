6779a55ce89eef05e6b49af71da59579829cdc09
andrr64 <derzandrr03@gmail.com>
2023-12-12 23:14:50 +0700

    update UI/Wallet

--- keuanganku/lib/app/main/wallet/wallet.dart
+++ keuanganku/lib/app/main/wallet/wallet.dart
@@ -1,22 +1,71 @@
 import 'package:flutter/material.dart';
+import 'package:keuanganku/app/app_colors.dart';
+import 'package:keuanganku/app/main/beranda/beranda.dart';
+import 'package:keuanganku/app/main/beranda/widgets/list_wallet/list_wallet.dart';
+import 'package:keuanganku/app/main/pengeluaran/pengeluaran.dart';
+import 'package:keuanganku/app/reusable%20widgets/k_app_bar/k_app_bar.dart';
+import 'package:keuanganku/app/state_bridge.dart';
+import 'package:keuanganku/util/dummy.dart';
 
 class Data {
   final Color backgroundColor = Colors.white;
 }
 
 class HalamanWallet extends StatefulWidget {
-  HalamanWallet({super.key});
+  HalamanWallet({super.key, required this.parentScaffoldKey});
   final Data data = Data();
-
+  static StateBridge state = StateBridge();
+  final GlobalKey<ScaffoldState> parentScaffoldKey;
   @override
   State<HalamanWallet> createState() => _HalamanWalletState();
 }
 
 class _HalamanWalletState extends State<HalamanWallet> {
   @override
+  void initState() {
+    super.initState();
+    HalamanWallet.state.init(() {setState(() {
+      
+    });});
+  }
+
+  @override
   Widget build(BuildContext context) {
+    // Widgets
+    Widget drawerButton(){
+      return GestureDetector(
+        onTap: (){
+          widget.parentScaffoldKey.currentState!.openDrawer();
+        },
+        child: const Icon(
+          Icons.menu, 
+          size: 30, 
+          color: Colors.white,
+        ),
+      );
+    }
+
     return Scaffold(
-      backgroundColor: widget.data.backgroundColor,
+      backgroundColor: ApplicationColors.primary,
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            dummyPadding(height: 50),
+            KPageAppBar(title: "Wallet", menuButton: drawerButton()),
+            dummyPadding(height: 25),
+            ListWallet(
+              updateState: (){
+                HalamanBeranda.state.update!();
+                HalamanWallet.state.update!();
+                HalamanPengeluaran.state.update!();
+              },
+            ),
+            dummyPadding(height: 100),
+          ],
+        ),
+      )
     );
   }
 }
