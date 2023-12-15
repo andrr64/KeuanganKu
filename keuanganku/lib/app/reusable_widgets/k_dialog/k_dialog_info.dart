7879a187ea982f871af1966237c31eafa6a7881d
andrr64 <derzandrr03@gmail.com>
2023-12-15 08:26:09 +0700

    Add KDialogInfo

---
+++ keuanganku/lib/app/reusable_widgets/k_dialog/k_dialog_info.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:keuanganku/app/reusable_widgets/k_button/k_button.dart';
+import 'package:keuanganku/enum/status.dart';
+
+class KDialogInfo{
+  final String title;
+  final String info;
+  final Pesan jenisPesan;
+  BuildContext? dialogContext;
+
+  KDialogInfo({
+    required this.title,
+    required this.info,
+    required this.jenisPesan
+  });
+
+
+  tampilkanDialog(BuildContext context){
+    showDialog(context: context, builder: (_){
+      dialogContext = _;
+      return  AlertDialog(
+        title: Text(title),
+        content: Text(info),
+        actions: [
+          KButton(onTap: (){Navigator.pop(_);}, title: "Ok", icon: const SizedBox())
+        ],
+      );
+    });
+  }
+}
