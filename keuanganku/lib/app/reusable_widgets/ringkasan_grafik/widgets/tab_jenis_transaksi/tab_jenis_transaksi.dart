29cf504dcf0b8fb9ccd7fb0b4bf91a62bae31a7f
andrr64 <derzandrr03@gmail.com>
2023-12-16 12:12:33 +0700

    delete unused file

--- keuanganku/lib/app/reusable_widgets/ringkasan_grafik/widgets/tab_jenis_transaksi/tab_jenis_transaksi.dart
+++ keuanganku/lib/app/reusable_widgets/ringkasan_grafik/widgets/tab_jenis_transaksi/tab_jenis_transaksi.dart
@@ -1,107 +1,107 @@
-import 'package:flutter/material.dart';
-import 'package:keuanganku/enum/data_transaksi.dart';
-import 'package:keuanganku/app/app_colors.dart';
-import 'package:keuanganku/app/reusable_widgets/ringkasan_grafik/ringkasan_grafik.dart' as ringkasan_grafik;
+// import 'package:flutter/material.dart';
+// import 'package:keuanganku/enum/data_transaksi.dart';
+// import 'package:keuanganku/app/app_colors.dart';
+// import 'package:keuanganku/app/reusable_widgets/ringkasan_grafik/ringkasan_grafik.dart' as ringkasan_grafik;
 
-class Properties {
-  final List<String> listTabJenisTransaksi = [
-    "Pemasukan",
-    "Pengeluaran"
-  ];
-  double  posisiGarisTabJenisTransaksi(indeksTab, panjangGarisTab) => panjangGarisTab * indeksTab;
-}
+// class Properties {
+//   final List<String> listTabJenisTransaksi = [
+//     "Pemasukan",
+//     "Pengeluaran"
+//   ];
+//   double  posisiGarisTabJenisTransaksi(indeksTab, panjangGarisTab) => panjangGarisTab * indeksTab;
+// }
 
-class TabJenisTransaksi extends StatelessWidget {
-  TabJenisTransaksi({super.key, required this.indeksTab, required this.data, required this.onUpdate});
-  final ringkasan_grafik.RuCRingkasanGrafikData data;
-  final int indeksTab;
-  final Properties properties = Properties();
-  final VoidCallback onUpdate;
+// class TabJenisTransaksi extends StatelessWidget {
+//   TabJenisTransaksi({super.key, required this.indeksTab, required this.data, required this.onUpdate});
+//   final ringkasan_grafik.RuCRingkasanGrafikData data;
+//   final int indeksTab;
+//   final Properties properties = Properties();
+//   final VoidCallback onUpdate;
 
-  @override
-  Widget build(BuildContext context) {
-    var size = MediaQuery.sizeOf(context);
-    var panjangGarisTab = size.width * 0.8 / 2;
+//   @override
+//   Widget build(BuildContext context) {
+//     var size = MediaQuery.sizeOf(context);
+//     var panjangGarisTab = size.width * 0.8 / 2;
 
-    // EVENTS
-    void ketikaTabBerubah(int index){
-      if (data.indeksTabJenisTransaksi == index) return;
-      data.indeksTabJenisTransaksi = index;
-      switch (index) {
-        case 0:
-          data.jenisTransaksi = JenisTransaksi.Pemasukan;
-          break;
-        case 1:
-          data.jenisTransaksi = JenisTransaksi.Pengeluaran;
-          break;
-      }
-      onUpdate();
-    }  
+//     // EVENTS
+//     void ketikaTabBerubah(int index){
+//       if (data.indeksTabJenisTransaksi == index) return;
+//       data.indeksTabJenisTransaksi = index;
+//       switch (index) {
+//         case 0:
+//           data.jenisTransaksi = JenisTransaksi.Pemasukan;
+//           break;
+//         case 1:
+//           data.jenisTransaksi = JenisTransaksi.Pengeluaran;
+//           break;
+//       }
+//       onUpdate();
+//     }  
 
-    // WIDGETS
-    widgetTeks(){
-      return 
-      SizedBox(
-        width: size.width*0.9,
-        height: 27.5,
-        child: ListView.builder(
-          scrollDirection: Axis.horizontal,
-          itemCount:properties.listTabJenisTransaksi.length,
-          physics: const BouncingScrollPhysics(),
-          itemBuilder: (context, index) {
-            return GestureDetector(
-              onTap: (){
-                ketikaTabBerubah(index);
-              },
-              child: SizedBox(
-                width:panjangGarisTab,
-                child: Text(
-                  properties.listTabJenisTransaksi[index],
-                  style: TextStyle(
-                    fontFamily: "QuickSand_Bold",
-                    fontSize: 20,
-                    color:data.indeksTabJenisTransaksi == index? ApplicationColors.primary : ApplicationColors.primaryColorWidthPercentage(percentage: 50 )
-                  ),
-                ),
-              )
-            );
-          }
-        ),
-      );
-    }
-    widgetGaris(){
-      return            
-      AnimatedPositioned(
-        duration: const Duration(milliseconds: 250),
-        bottom: 0,
-        left:properties.posisiGarisTabJenisTransaksi(panjangGarisTab, indeksTab),
-        curve: Curves.easeInOutQuint,
-        child: AnimatedContainer(
-          duration: const Duration(milliseconds: 500),
-          width: panjangGarisTab - 10,
-          height: 2,
-          curve: Curves.ease,
-          decoration: BoxDecoration(
-            color: ApplicationColors.secondaryOrange,
-            borderRadius: BorderRadius.circular(15)
-          ),
-        ),
-      );
-    }
+//     // WIDGETS
+//     widgetTeks(){
+//       return 
+//       SizedBox(
+//         width: size.width*0.9,
+//         height: 27.5,
+//         child: ListView.builder(
+//           scrollDirection: Axis.horizontal,
+//           itemCount:properties.listTabJenisTransaksi.length,
+//           physics: const BouncingScrollPhysics(),
+//           itemBuilder: (context, index) {
+//             return GestureDetector(
+//               onTap: (){
+//                 ketikaTabBerubah(index);
+//               },
+//               child: SizedBox(
+//                 width:panjangGarisTab,
+//                 child: Text(
+//                   properties.listTabJenisTransaksi[index],
+//                   style: TextStyle(
+//                     fontFamily: "QuickSand_Bold",
+//                     fontSize: 20,
+//                     color:data.indeksTabJenisTransaksi == index? ApplicationColors.primary : ApplicationColors.primaryColorWidthPercentage(percentage: 50 )
+//                   ),
+//                 ),
+//               )
+//             );
+//           }
+//         ),
+//       );
+//     }
+//     widgetGaris(){
+//       return            
+//       AnimatedPositioned(
+//         duration: const Duration(milliseconds: 250),
+//         bottom: 0,
+//         left:properties.posisiGarisTabJenisTransaksi(panjangGarisTab, indeksTab),
+//         curve: Curves.easeInOutQuint,
+//         child: AnimatedContainer(
+//           duration: const Duration(milliseconds: 500),
+//           width: panjangGarisTab - 10,
+//           height: 2,
+//           curve: Curves.ease,
+//           decoration: BoxDecoration(
+//             color: ApplicationColors.secondaryOrange,
+//             borderRadius: BorderRadius.circular(15)
+//           ),
+//         ),
+//       );
+//     }
 
-    return 
-    SizedBox(
-      width: size.width * 0.9,
-      child: Column(
-        children: [
-          Stack(
-            children: [
-              widgetTeks(),
-              widgetGaris()
-            ]
-          ),
-        ],
-      ),
-    );
-  }
-}
+//     return 
+//     SizedBox(
+//       width: size.width * 0.9,
+//       child: Column(
+//         children: [
+//           Stack(
+//             children: [
+//               widgetTeks(),
+//               widgetGaris()
+//             ]
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
--- keuanganku/lib/app/reusable_widgets/ringkasan_grafik/widgets/tab_waktu_transaksi/tab_waktu_transaksi.dart
+++ keuanganku/lib/app/reusable_widgets/ringkasan_grafik/widgets/tab_waktu_transaksi/tab_waktu_transaksi.dart
@@ -1,106 +1,106 @@
 
-import 'package:flutter/material.dart';
-import 'package:keuanganku/enum/data_transaksi.dart';
-import 'package:keuanganku/app/app_colors.dart';
-import 'package:keuanganku/app/reusable_widgets/ringkasan_grafik/ringkasan_grafik.dart' as ringkasan_grafik;
+// import 'package:flutter/material.dart';
+// import 'package:keuanganku/enum/data_transaksi.dart';
+// import 'package:keuanganku/app/app_colors.dart';
+// import 'package:keuanganku/app/reusable_widgets/ringkasan_grafik/ringkasan_grafik.dart' as ringkasan_grafik;
 
-class Properties {
-  final List<String> listTabWaktuTransaksi = [
-    "1M", "1Y"
-  ];
-  double panjangGarisTabWaktuTransaksi = 45;
-  double posisiGarisTabWaktuTransaksi(indeksTabWaktuTransaksi) => indeksTabWaktuTransaksi * panjangGarisTabWaktuTransaksi;
-}
+// class Properties {
+//   final List<String> listTabWaktuTransaksi = [
+//     "1M", "1Y"
+//   ];
+//   double panjangGarisTabWaktuTransaksi = 45;
+//   double posisiGarisTabWaktuTransaksi(indeksTabWaktuTransaksi) => indeksTabWaktuTransaksi * panjangGarisTabWaktuTransaksi;
+// }
 
-class TabWaktuTransaksi{
-  int indeksWaktuTransaksi;
-  VoidCallback onUpdate;
-  TabWaktuTransaksi({required this.indeksWaktuTransaksi, required this.data, required this.onUpdate});
-  ringkasan_grafik.RuCRingkasanGrafikData data = ringkasan_grafik.RuCRingkasanGrafikData();
-  final Properties properties = Properties();
+// class TabWaktuTransaksi{
+//   int indeksWaktuTransaksi;
+//   VoidCallback onUpdate;
+//   TabWaktuTransaksi({required this.indeksWaktuTransaksi, required this.data, required this.onUpdate});
+//   ringkasan_grafik.RuCRingkasanGrafikData data = ringkasan_grafik.RuCRingkasanGrafikData();
+//   final Properties properties = Properties();
 
-  Widget getWidget(BuildContext context){
-    void waktuTransaksiBerubah(int index){
-      if (data.indeksTabWaktuTransaksi == index) return;
-      data.indeksTabWaktuTransaksi = index;
-      switch (index) {
-        case 0:
-          data.waktuTransaksi = WaktuTransaksi.Mingguan;
-          break;
-        case 1:
-          data.waktuTransaksi = WaktuTransaksi.Tahunan;
-          break;
-        default:
-      }
-      onUpdate();
-    }
-    var size = MediaQuery.sizeOf(context);
+//   Widget getWidget(BuildContext context){
+//     void waktuTransaksiBerubah(int index){
+//       if (data.indeksTabWaktuTransaksi == index) return;
+//       data.indeksTabWaktuTransaksi = index;
+//       switch (index) {
+//         case 0:
+//           data.waktuTransaksi = WaktuTransaksi.Mingguan;
+//           break;
+//         case 1:
+//           data.waktuTransaksi = WaktuTransaksi.Tahunan;
+//           break;
+//         default:
+//       }
+//       onUpdate();
+//     }
+//     var size = MediaQuery.sizeOf(context);
 
-    // WIDGETS
-    widgetTeksTab(){
-      return  
-      SizedBox(
-        width: size.width*0.9,
-        height: 27.5,
-        child: ListView.builder(
-          scrollDirection: Axis.horizontal,
-          itemCount:properties.listTabWaktuTransaksi.length,
-          physics: const BouncingScrollPhysics(),
-          itemBuilder: (context, index) {
-            return GestureDetector(
-              onTap: (){
-                waktuTransaksiBerubah(index);
-              },
-              child: Container(
-                alignment: Alignment.centerLeft,
-                width: properties.panjangGarisTabWaktuTransaksi,
-                child: Text(
-                properties.listTabWaktuTransaksi[index],
-                  style: TextStyle(
-                    fontFamily: "QuickSand_Bold",
-                    fontSize: 14,
-                    color:data.indeksTabWaktuTransaksi == index? ApplicationColors.primary : ApplicationColors.primaryColorWidthPercentage(percentage: 50 )
-                  ),
-                ),
-              )
-            );
-          }
-        ),
-      );          
-    }
-    widgetGarisTab(){
-      return 
-      AnimatedPositioned(
-        duration: const Duration(milliseconds: 250),
-        bottom: 0,
-        left:properties.posisiGarisTabWaktuTransaksi(indeksWaktuTransaksi),
-        curve: Curves.easeInOutQuint,
-        child: AnimatedContainer(
-          duration: const Duration(milliseconds: 500),
-          width: properties.panjangGarisTabWaktuTransaksi-15,
-          height: 2,
-          curve: Curves.ease,
-          decoration: BoxDecoration(
-            color: ApplicationColors.secondaryOrange,
-            borderRadius: BorderRadius.circular(15)
-          ),
-        ),
-      );
-    }
+//     // WIDGETS
+//     widgetTeksTab(){
+//       return  
+//       SizedBox(
+//         width: size.width*0.9,
+//         height: 27.5,
+//         child: ListView.builder(
+//           scrollDirection: Axis.horizontal,
+//           itemCount:properties.listTabWaktuTransaksi.length,
+//           physics: const BouncingScrollPhysics(),
+//           itemBuilder: (context, index) {
+//             return GestureDetector(
+//               onTap: (){
+//                 waktuTransaksiBerubah(index);
+//               },
+//               child: Container(
+//                 alignment: Alignment.centerLeft,
+//                 width: properties.panjangGarisTabWaktuTransaksi,
+//                 child: Text(
+//                 properties.listTabWaktuTransaksi[index],
+//                   style: TextStyle(
+//                     fontFamily: "QuickSand_Bold",
+//                     fontSize: 14,
+//                     color:data.indeksTabWaktuTransaksi == index? ApplicationColors.primary : ApplicationColors.primaryColorWidthPercentage(percentage: 50 )
+//                   ),
+//                 ),
+//               )
+//             );
+//           }
+//         ),
+//       );          
+//     }
+//     widgetGarisTab(){
+//       return 
+//       AnimatedPositioned(
+//         duration: const Duration(milliseconds: 250),
+//         bottom: 0,
+//         left:properties.posisiGarisTabWaktuTransaksi(indeksWaktuTransaksi),
+//         curve: Curves.easeInOutQuint,
+//         child: AnimatedContainer(
+//           duration: const Duration(milliseconds: 500),
+//           width: properties.panjangGarisTabWaktuTransaksi-15,
+//           height: 2,
+//           curve: Curves.ease,
+//           decoration: BoxDecoration(
+//             color: ApplicationColors.secondaryOrange,
+//             borderRadius: BorderRadius.circular(15)
+//           ),
+//         ),
+//       );
+//     }
 
-    return 
-    SizedBox(
-      width: size.width * 0.9,
-      child: Column(
-        children: [
-          Stack(
-            children: [
-              widgetTeksTab(),
-              widgetGarisTab()
-            ]
-          ),
-        ],
-      ),
-    );
-  }
-}
+//     return 
+//     SizedBox(
+//       width: size.width * 0.9,
+//       child: Column(
+//         children: [
+//           Stack(
+//             children: [
+//               widgetTeksTab(),
+//               widgetGarisTab()
+//             ]
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
