e620b1a62379fdb4c78ba867548fee7e6a219921
andrr64 <derzandrr03@gmail.com>
2023-12-19 01:10:38 +0700

    menambah generate_color.dart

---
+++ keuanganku/lib/util/generate_color.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+Color mapValueToColor(double value) {
+  if (value == -1) {
+    return Colors.grey;
+  }
+  if (value < 1.0 || value > 5.0) {
+    throw ArgumentError('Nilai harus di antara 1 dan 5');
+  }
+
+  // Hitung nilai Hue untuk gradient dari merah ke hijau
+  double hue = (value - 1.0) / 4.0 * 120.0;
+
+  // Ubah nilai Hue menjadi warna menggunakan HSLColor
+  HSLColor color = HSLColor.fromAHSL(1.0, hue, 1.0, 0.5);
+
+  // Konversi HSLColor menjadi Color
+  return color.toColor();
+}
