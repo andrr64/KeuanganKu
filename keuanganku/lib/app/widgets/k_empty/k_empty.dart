d5b5b8ac11fad5909f53a5bd5b56e94fa7bc0a73
andrr64 <derzandrr03@gmail.com>
2023-12-19 01:01:55 +0700

    menghapus animasi lottiefile * berat euy kalau pake animasi

--- keuanganku/lib/app/widgets/k_empty/k_empty.dart
+++ keuanganku/lib/app/widgets/k_empty/k_empty.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:keuanganku/util/dummy.dart';
 import 'package:keuanganku/util/font_style.dart';
-import 'package:lottie/lottie.dart';
 
 class KEmpty extends StatelessWidget {
   const KEmpty({super.key, this.variant});
@@ -11,12 +10,12 @@ class KEmpty extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    const listVariant = [
-      "assets/lotties/animation/empty.json",
-      "assets/lotties/animation/empty1.json",
-      "assets/lotties/animation/empty2.json",
-      "assets/lotties/animation/empty3.json",
-    ];
+    // const listVariant = [
+    //   "assets/lotties/animation/empty.json",
+    //   "assets/lotties/animation/empty1.json",
+    //   "assets/lotties/animation/empty2.json",
+    //   "assets/lotties/animation/empty3.json",
+    // ];
 
     return makeCenterWithRow(
       child: Padding(
@@ -27,11 +26,6 @@ class KEmpty extends StatelessWidget {
             child: Column(
               children: [
                 Text("Nothing in here...", style: kFontStyle(fontSize: 15, color: Colors.black45),),
-                Lottie.asset(
-                  variant == null? listVariant[2] : listVariant[variant!],
-                  repeat: false,
-                  height: 150
-                ),
               ],
             ),
           ),
