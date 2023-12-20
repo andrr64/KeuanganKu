55a3938ac59f8448f959d8e5bc8a560be9528c56
andrr64 <derzandrr03@gmail.com>
2023-12-20 08:42:08 +0700

    menambah fungsi dummyWidth

--- keuanganku/lib/util/dummy.dart
+++ keuanganku/lib/util/dummy.dart
@@ -4,6 +4,10 @@ dummyHeight({double height = 15}){
   return SizedBox(height: height,);
 }
 
+dummyWidth(double width){
+  return SizedBox(width: width,);
+}
+
 makeCenterWithRow({required Widget child}){
   return Row(
     crossAxisAlignment: CrossAxisAlignment.center,
