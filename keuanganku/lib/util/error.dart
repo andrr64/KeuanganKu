3d6a0eae65602ae52c26e01398db0a0a7043c304
andrr64 <derzandrr03@gmail.com>
2023-12-20 09:27:30 +0700

    menanbah enum exception untuk mempermudah pembacaan kode

---
+++ keuanganku/lib/util/error.dart
@@ -0,0 +1,16 @@
+// ignore_for_file: constant_identifier_names
+
+enum SQLError {
+  Read,
+  Create,
+  Insert,
+  Delete
+}
+
+enum ValidatorError {
+  InvalidNumber,
+  OverflowNumber,
+  OverflowText,
+  IfCondition,
+  LessThanError
+}
