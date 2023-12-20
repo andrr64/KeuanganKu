8a913b9cfaed5a8f381ca2945ef6eaa4b75c0b86
andrr64 <derzandrr03@gmail.com>
2023-12-20 23:32:06 +0700

    refaktorisasi dan merapihkan kode

--- keuanganku/lib/app/routes/main/beranda/beranda.dart
+++ keuanganku/lib/app/routes/main/beranda/beranda.dart
@@ -7,8 +7,11 @@ import 'package:keuanganku/app/routes/main/wallet/wallet.dart';
 import 'package:keuanganku/app/routes/main/wallet/widgets/list_wallet/list_wallet.dart';
 import 'package:keuanganku/app/routes/main/beranda/widgets/statistik/statistik.dart';
 import 'package:keuanganku/app/state_bridge.dart';
+import 'package:keuanganku/app/widgets/k_future_builder/k_future.dart';
 import 'package:keuanganku/database/helper/user_data.dart';
 import 'package:keuanganku/database/helper/wallet.dart';
+import 'package:keuanganku/database/model/user_data.dart';
+import 'package:keuanganku/k_typedef.dart';
 import 'package:keuanganku/main.dart';
 import 'package:keuanganku/util/dummy.dart';
 import 'package:keuanganku/app/routes/main/beranda/widgets/distribusi/distribusi_transaksi.dart' as distribusi_tx;
@@ -33,32 +36,38 @@ class HalamanBeranda extends StatefulWidget {
 
 class _HalamanBerandaState extends State<HalamanBeranda> {
 
-  KEventHandler callback(){
+  KEventHandler callback    () {
     updateState();
     HalamanPengeluaran.state.update();
     HalamanWallet.state.update();
   }
-
-  void updateState() {
+  KEventHandler updateState () {
     setState(() {
 
     });
   }
 
-  Widget drawerButton(){
-    return GestureDetector(
-      onTap: (){
-        widget.parentScaffoldKey.currentState!.openDrawer();
-      },
-      child: const Icon(
-        Icons.menu, 
-        size: 30, 
-        color: Colors.white,
-      ),
+  KWidget   ringkasanGrafik     () {
+    return SingleChildScrollView(
+        scrollDirection: Axis.horizontal,
+        child: IntrinsicWidth(
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              dummyWidth(25,),
+              Statistik(widgetData: HalamanBeranda.widgetData.wxDataStatistik,),
+              dummyWidth(15),
+              DistribusiTransaksi(
+                widgetData: HalamanBeranda.widgetData.wxDataDistribusiTransaksi,
+                getter: () => HalamanBeranda.widgetData.wxDataDistribusiTransaksi.getData(),
+              ),
+              dummyWidth(25,),
+            ],
+          ),
+        )
     );
   }
-   
-  Widget listWallet() {
+  KWidget   listWallet          () {
     void callback(){
       updateState();
       HalamanWallet.state.update();
@@ -78,34 +87,37 @@ class _HalamanBerandaState extends State<HalamanBeranda> {
       },
     );
   }
-  Widget getUsername() {
-    return FutureBuilder(
-      future: SQLHelperUserData().readAll(db.database), 
-      builder: (_, snapshot){
-        if (snapshot.connectionState == ConnectionState.waiting){
-          return makeCenterWithRow(child: const CircularProgressIndicator());
-        } else {
-          String username = "";
-          const String defaultUserName = "User";
-          if (snapshot.hasError){
-            username = defaultUserName;
-          } else {
-            username = snapshot.data![0].username ?? defaultUserName;
-          }
-          
-          return Padding(
-            padding: const EdgeInsets.only(left: 25),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text("Hai,", style: kFontStyle(fontSize: 16, color: Colors.white, family: "QuickSand_Medium"),),
-                Text(username, style: kFontStyle(fontSize: 26, color: Colors.white),),
-              ],
-            ),
-          );
-        }
+  KWidget   headingUsername     () {
+    Widget buildHeadingUsername(SQLModelUserdata? data){
+      String username = "User";
+      if (data != null){
+        username = data.username ?? "User";
       }
+
+      return Padding(
+        padding: const EdgeInsets.only(left: 25),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text("Hai,", style: kFontStyle(fontSize: 16, color: Colors.white, family: "QuickSand_Medium"),),
+            Text(username, style: kFontStyle(fontSize: 26, color: Colors.white),),
+          ],
+        ),
+      );
+    }
+
+    return KFutureBuilder.build(
+        future: SQLHelperUserData().readById(db.database, 1),
+        whenError: buildHeadingUsername(null),
+        whenWaiting: makeCenterWithRow(child: const CircularProgressIndicator()),
+        whenSuccess: (data) => buildHeadingUsername(data)
+    );
+  }
+  KWidget   listExpenseLimiter  (){
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 25),
+      child: ListExpenseLimiter(callback: callback),
     );
   }
 
@@ -116,35 +128,13 @@ class _HalamanBerandaState extends State<HalamanBeranda> {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          getUsername(),
+          headingUsername(),
           dummyHeight(height: paddingBottom),
           listWallet(),
           dummyHeight(height: paddingBottom),
-          SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
-            child: IntrinsicWidth(
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const SizedBox(width: 25,),
-                  Statistik(
-                    widgetData: HalamanBeranda.widgetData.wxDataStatistik,
-                  ),
-                  const SizedBox(width: 15,),
-                  DistribusiTransaksi(
-                    widgetData: HalamanBeranda.widgetData.wxDataDistribusiTransaksi,
-                    getter: () => HalamanBeranda.widgetData.wxDataDistribusiTransaksi.getData(),
-                  ),
-                  const SizedBox(width: 25,),
-                ],  
-              ),
-            )
-          ),
+          listExpenseLimiter(),
           dummyHeight(height: paddingBottom),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 25),
-            child: ListExpenseLimiter(callback: callback),
-          ),
+          ringkasanGrafik(),
           dummyHeight(height: 25),
         ]
       ),
