3d465473c35dd99bfe77cb7845de3ee239f650b3
andrr64 <derzandrr03@gmail.com>
2023-12-20 08:41:51 +0700

    update KFuture

--- keuanganku/lib/app/widgets/k_future_builder/k_future.dart
+++ keuanganku/lib/app/widgets/k_future_builder/k_future.dart
@@ -1,33 +1,23 @@
 import 'package:flutter/material.dart';
 
-/// Class ini digunakan untuk mempermudah penggunaan FutureBuilder
 class KFutureBuilder {
   static Widget build<T>({
-    required BuildContext context,
     required Future<T> future,
-    required Widget Function(T) buildWhenSuccess,
-    required Widget Function() buildWhenEmpty,
-    required Widget Function() buildWhenError,
-    Widget Function()? buildWhenWaiting,
-    bool? isJustVoid,
-  }) {
+    required Widget whenError,
+    required Widget Function(T) whenSuccess,
+    Widget? whenWaiting,
+
+  }){
     return FutureBuilder(
-      future: future,
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return buildWhenWaiting?.call() ?? const CircularProgressIndicator();
-        } else if (snapshot.hasError) {
-          return buildWhenError();
-        } else if (snapshot.hasData || isJustVoid == true) {
-          if (snapshot.data == null && isJustVoid != true) {
-            return buildWhenEmpty();
+        future: future,
+        builder: (_, snapshot){
+          if (snapshot.connectionState == ConnectionState.waiting){
+            return whenWaiting ?? const CircularProgressIndicator();
+          } else if (snapshot.hasError){
+            return whenError;
           } else {
-            return buildWhenSuccess(snapshot.data as T);
+            return whenSuccess(snapshot.data!);
           }
-        } else {
-          return buildWhenEmpty();
-        }
-      },
-    );
+        });
   }
 }
