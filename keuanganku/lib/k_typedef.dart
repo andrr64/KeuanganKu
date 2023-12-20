79ca818e6fa506f825c47ad7f81b3b992dcd6274
andrr64 <derzandrr03@gmail.com>
2023-12-20 17:56:33 +0700

    update typedef KValidator

--- keuanganku/lib/k_typedef.dart
+++ keuanganku/lib/k_typedef.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 typedef KWidget = Widget;
 typedef KApplicationBar = PreferredSizeWidget;
-typedef KVoidValidator = void;
+typedef KInputValidator = int;
+typedef KValidator = dynamic;
 typedef KFormWidget = Widget;
