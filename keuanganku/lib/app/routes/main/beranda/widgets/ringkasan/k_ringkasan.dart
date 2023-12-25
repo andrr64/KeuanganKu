fde7e324e200fe8239779b8a39014b8e38790d71
andrr64 <derzandrr03@gmail.com>
2023-12-25 11:23:16 +0700

    penyederhanaan dan peningkatan performa widget

--- keuanganku/lib/app/routes/main/beranda/widgets/ringkasan/k_ringkasan.dart
+++ keuanganku/lib/app/routes/main/beranda/widgets/ringkasan/k_ringkasan.dart
@@ -6,17 +6,35 @@ import 'package:keuanganku/app/widgets/k_dropdown_menu/k_drodpown_menu.dart';
 import 'package:keuanganku/app/widgets/k_future_builder/k_future.dart';
 import 'package:keuanganku/database/helper/expense.dart';
 import 'package:keuanganku/database/helper/income.dart';
-import 'package:keuanganku/database/helper/wallet.dart';
 import 'package:keuanganku/database/model/expense.dart';
+import 'package:keuanganku/database/model/income.dart';
 import 'package:keuanganku/enum/data_transaksi.dart';
 import 'package:keuanganku/k_typedef.dart';
 import 'package:keuanganku/main.dart';
+import 'package:keuanganku/util/dummy.dart';
 import 'package:keuanganku/util/font_style.dart';
 import 'package:keuanganku/util/get_currency.dart';
 import 'package:keuanganku/util/vector_operation.dart';
 
 class Data {
   WaktuTransaksi waktuTransaksi = WaktuTransaksi.Bulanan;
+  List<SQLModelExpense> _listPengeluaran = [];
+  List<SQLModelIncome> _listPemasukan = [];
+  double _totalPengeluaran = 0;
+  double _totalPemasukan = 0;
+
+  Future updateData() async{
+    _listPemasukan = await SQLHelperIncome().readByWaktuAndSortir(waktuTransaksi, SortirTransaksi.Default, db: db.database);
+    _listPengeluaran = await SQLHelperExpense().readByWaktuAndSortir(waktuTransaksi,SortirTransaksi.Default, db: db.database);
+    _totalPemasukan = sumList(_listPemasukan.map((e) => e.nilai).toList());
+    _totalPengeluaran = sumList(_listPengeluaran.map((e) => e.nilai).toList());
+  }
+
+  List<SQLModelIncome> get listPemasukan => _listPemasukan;
+  List<SQLModelExpense> get listPengeluaran => _listPengeluaran;
+  double get totalPemasukan => _totalPemasukan;
+  double get totalPengeluaran => _totalPengeluaran; 
+
   String getInfoWaktu(waktuTransaksiX){
     switch(waktuTransaksiX){
       case WaktuTransaksi.Bulanan:
@@ -42,84 +60,60 @@ class KRingkasan extends StatefulWidget {
 
 class _KRingkasanState extends State<KRingkasan> {
   KWidget     totalPemasukan    (BuildContext context){
-    Future<double> getData() async {
-      return sumList((await SQLHelperIncome().readByWaktuAndSortir(widget.widgetData.waktuTransaksi, SortirTransaksi.Default, db: db.database)).map((e) => e.nilai).toList());
-    }
-    Widget build(double value){
-      double panjangContainerUang = MediaQuery.sizeOf(context).width * 0.4;
-      return Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text("Pemasukan", style: kFontStyle(fontSize: 16, family: "QuickSand_Medium"),),
-              Container(
-                  alignment: Alignment.centerRight,
-                  width: panjangContainerUang,
-                  child: Text(
-                    formatCurrency(value),
-                    style: kFontStyle(fontSize: 16),
-                    overflow: TextOverflow.ellipsis,
-                  )
-              )
-            ],
-          )
-        ],
-      );
-    }
-    return KFutureBuilder.build(
-        future: getData(),
-        whenWaiting: build(0),
-        whenError: build(0),
-        whenSuccess: (value) => build(value)
-    );
-  }
-  KWidget     totalPengeluaran  (BuildContext context){
-    Future getData() async {
-      List<SQLModelExpense> seluruhPengeluaranBerdasarkanWaktu = await SQLHelperExpense().readByWaktu(widget.widgetData.waktuTransaksi, db: db.database);
-      double pengeluaranKeseluruhan = sumList(seluruhPengeluaranBerdasarkanWaktu.map((e) => e.nilai).toList());
-      return pengeluaranKeseluruhan;
-    }
-    Widget build(value){
-      double panjangContainerUang = MediaQuery.sizeOf(context).width * 0.4;
-      return Column(
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text("Pengeluaran", style: kFontStyle(fontSize: 16, family: "QuickSand_Medium"),),
-              Container(
+    double panjangContainerUang = MediaQuery.sizeOf(context).width * 0.4;
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text("Pemasukan", style: kFontStyle(fontSize: 16, family: "QuickSand_Medium"),),
+            Container(
                 alignment: Alignment.centerRight,
                 width: panjangContainerUang,
                 child: Text(
-                  formatCurrency(value),
+                  formatCurrency(widget.widgetData.totalPemasukan),
                   style: kFontStyle(fontSize: 16),
                   overflow: TextOverflow.ellipsis,
                 )
+            )
+          ],
+        )
+      ],
+    );
+  }
+  KWidget     totalPengeluaran  (BuildContext context){
+    double panjangContainerUang = MediaQuery.sizeOf(context).width * 0.4;
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text("Pengeluaran", style: kFontStyle(fontSize: 16, family: "QuickSand_Medium"),),
+            Container(
+              alignment: Alignment.centerRight,
+              width: panjangContainerUang,
+              child: Text(
+                formatCurrency(widget.widgetData.totalPengeluaran),
+                style: kFontStyle(fontSize: 16),
+                overflow: TextOverflow.ellipsis,
               )
-            ],
-          )
-        ],
-      );
-    }
-    return KFutureBuilder.build(
-        future: getData(), 
-        whenWaiting: build(0.0),
-        whenError: build(0.0),
-        whenSuccess: (value) => build(value as double)
+            )
+          ],
+        )
+      ],
     );
   }
   KWidget     dropdownWaktu     (BuildContext context){
     List<DropdownMenuItem<WaktuTransaksi>> items(){
       return WaktuTransaksi.values.map((e) =>
           DropdownMenuItem(
+            value: e,
             child: Text(
               widget.widgetData.getInfoWaktu(e),
               style: kFontStyle(fontSize: 15, family: "QuickSand_Medium"),
-            ),
-            value: e,)
+            ),)
       ).toList();
     }
 
@@ -139,62 +133,37 @@ class _KRingkasanState extends State<KRingkasan> {
     );
   }
   KWidget     delta             (BuildContext context){
-    Future<Map<String, dynamic>> getData() async {
-      List<SQLModelExpense> seluruhPengeluaranBerdasarkanWaktu = await SQLHelperExpense().readByWaktu(widget.widgetData.waktuTransaksi, db: db.database);
-      /// Pengeluaran dalam kurun waktu tertentu
-      double pengeluaranKeseluruhan = sumList(seluruhPengeluaranBerdasarkanWaktu.map((e) => e.nilai).toList());
-
-      /// Dana dalam kurun waktu tertentu
-      double danaKeseluruhan = await SQLHelperWallet().readSeluruhTotalUangTersedia();
-      double delta = danaKeseluruhan - pengeluaranKeseluruhan;
-      /// Selisih dalam kurun waktu tertentu
-      return {
-        'delta': delta,
-        'color': delta < 0? Colors.red : delta == 0? ApplicationColors.primary : Colors.green
-      };
-    }
-    Widget build(value){
-      double panjangContainerUang = MediaQuery.sizeOf(context).width * 0.4;
-      return Column(
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text("Delta", style: kFontStyle(fontSize: 16, family: "QuickSand_Medium"),),
-              Container(
-                  alignment: Alignment.centerRight,
-                  width: panjangContainerUang,
-                  child: Text(
-                    formatCurrency(value['delta']),
-                    style: kFontStyle(fontSize: 16, color: value['color']),
-                    overflow: TextOverflow.ellipsis,
-                  )
-              )
-            ],
-          )
-        ],
-      );
-    }
-    return KFutureBuilder.build<Map<String, dynamic>>(
-        future: getData(),
-        whenWaiting: build({
-          'delta' : 0.0,
-          'color': ApplicationColors.primary
-        }),
-        whenError: build({
-          'delta' : 0.0,
-          'color': ApplicationColors.primary
-        }),
-        whenSuccess: (value) => build(value)
+    double delta = widget.widgetData.totalPemasukan - widget.widgetData.totalPengeluaran;
+    final color = delta < 0? Colors.red : delta == 0? ApplicationColors.primary : Colors.green;
+    double panjangContainerUang = MediaQuery.sizeOf(context).width * 0.4;
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text("Delta", style: kFontStyle(fontSize: 16, family: "QuickSand_Medium"),),
+            Container(
+                alignment: Alignment.centerRight,
+                width: panjangContainerUang,
+                child: Text(
+                  formatCurrency(delta),
+                  style: kFontStyle(fontSize: 16, color: color),
+                  overflow: TextOverflow.ellipsis,
+                )
+            )
+          ],
+        )
+      ],
     );
   }
+  
   final icon = SvgPicture.asset("assets/icons/ringkasan.svg");
-
-  @override
-  Widget build(BuildContext context) {
+  
+  Widget buildBody(BuildContext context){
     return KCard(
         title: "Ringkasan",
         icon: icon,
+        button: dropdownWaktu(context),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
@@ -203,7 +172,16 @@ class _KRingkasanState extends State<KRingkasan> {
             delta(context)
           ],
         ),
-        button: dropdownWaktu(context),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return KFutureBuilder.build(
+      future: widget.widgetData.updateData(), 
+      whenError: makeCenterWithRow(child: const CircularProgressIndicator(color: Colors.white,)), 
+      whenWaiting: makeCenterWithRow(child: const CircularProgressIndicator(color: Colors.white,)),
+      whenSuccess: (val) => buildBody(context)
     );
   }
 }
