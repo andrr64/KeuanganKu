8c1f44cf6f612d3e5fa1b6d2e859e8bb836e0383
andrr64 <derzandrr03@gmail.com>
2023-12-27 18:03:05 +0700

    penambahan tombol ubah dan hapus wallet

--- keuanganku/lib/app/routes/main/wallet/pages/detail_wallet/detail_wallet.dart
+++ keuanganku/lib/app/routes/main/wallet/pages/detail_wallet/detail_wallet.dart
@@ -1,20 +1,21 @@
 // ignore_for_file: library_prefixes
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:keuanganku/app/app_colors.dart';
 import 'package:keuanganku/app/routes/main/beranda/widgets/distribusi/distribusi_transaksi.dart' as distribusiTx;
-import 'package:keuanganku/app/routes/main/pengeluaran/widgets/k_pengeluaran_item/k_pengeluaran_item.dart';
 import 'package:keuanganku/app/routes/main/pengeluaran/widgets/list_pengeluaran/list_pengeluaran.dart';
-import 'package:keuanganku/app/routes/main/wallet/widgets/k_pemasukan_item/k_pemasukan_item.dart';
 import 'package:keuanganku/app/routes/main/wallet/widgets/list_pemasukan/k_list_pemasukan.dart';
+import 'package:keuanganku/app/snack_bar.dart';
 import 'package:keuanganku/app/widgets/app_bar/app_bar.dart';
-import 'package:keuanganku/app/widgets/k_empty/k_empty.dart';
+import 'package:keuanganku/app/widgets/k_dialog/k_dialog_info.dart';
 import 'package:keuanganku/app/widgets/k_future_builder/k_future.dart';
 import 'package:keuanganku/database/helper/income.dart';
 import 'package:keuanganku/database/helper/expense.dart';
-import 'package:keuanganku/database/model/income.dart';
+import 'package:keuanganku/database/helper/wallet.dart';
 import 'package:keuanganku/database/model/expense.dart';
 import 'package:keuanganku/database/model/wallet.dart';
+import 'package:keuanganku/enum/status.dart';
 import 'package:keuanganku/k_typedef.dart';
 import 'package:keuanganku/main.dart';
 import 'package:keuanganku/util/dummy.dart';
@@ -48,36 +49,8 @@ class _DetailWalletState extends State<DetailWallet> {
   }
   
   // Builder
-  Widget buildListPemasukan(BuildContext context, List<SQLModelIncome> listPemasukan){
-    if (listPemasukan.isEmpty){
-      return makeCenterWithRow(child: const KEmpty());
-    }
-    return Column(
-          children: [
-            for(int i=0;i < listPemasukan.length; i++)
-              KPemasukanItem(
-                callback: callback,
-                size: Size(MediaQuery.sizeOf(context).width * 0.875, 40), 
-                pemasukan: listPemasukan[i]
-              ),
-          ],
-        );
-  }
-  Widget buildListPengeluaran(BuildContext context, List<SQLModelExpense> listPengeluaran){
-    if (listPengeluaran.isEmpty){
-      return makeCenterWithRow(child: const KEmpty());
-    }
 
-    return Column(
-      children: [
-        for(int i=0;i < listPengeluaran.length; i++)
-          KPengeluaranItem(pengeluaran: listPengeluaran[i],
-          callback: callback
-        ),
-      ],
-    );
-  }
-  Widget buildDistribusiPengeluaran(BuildContext context){
+  Widget buildDistribusiPengeluaran (BuildContext context){
     return makeCenterWithRow(
       child: distribusiTx.DistribusiTransaksi(
         widgetData: widget.data.distribusiTxData,
@@ -96,6 +69,10 @@ class _DetailWalletState extends State<DetailWallet> {
         listPengeluaran: const [], 
         callback: callback
       ), 
+      whenWaiting: ListPengeluaran(
+        listPengeluaran: const [], 
+        callback: callback
+      ),
       whenSuccess: (data){
         return ListPengeluaran(
           listPengeluaran: data, 
@@ -105,28 +82,25 @@ class _DetailWalletState extends State<DetailWallet> {
     );
   }
   KWidget         listPemasukan(BuildContext context){
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 25),
-      child: FutureBuilder(
-            future: SQLHelperIncome().readDataByWalletId(widget.wallet.id, db.database),
-            builder: (_, snapshot){
-              if (snapshot.connectionState == ConnectionState.waiting){
-                return makeCenterWithRow(child: const CircularProgressIndicator());
-              } else if (snapshot.hasError){
-                return makeCenterWithRow(child: const Text("Sadly, something wrong..."));
-              } else {
-                return KListPemasukan(
-                  listPemasukan: snapshot.data!, 
-                  callback: callback
-                );
-              }
+    return FutureBuilder(
+          future: SQLHelperIncome().readDataByWalletId(widget.wallet.id, db.database),
+          builder: (_, snapshot){
+            if (snapshot.connectionState == ConnectionState.waiting){
+              return makeCenterWithRow(child: const CircularProgressIndicator());
+            } else if (snapshot.hasError){
+              return makeCenterWithRow(child: const Text("Sadly, something wrong..."));
+            } else {
+              return KListPemasukan(
+                listPemasukan: snapshot.data!, 
+                callback: callback
+              );
             }
-        ),
-    );
+          }
+      );
   }
   KWidget         ringkasanWallet(BuildContext context){
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 25),
+      padding: const EdgeInsets.symmetric(vertical: 25),
       child: Container(
           alignment: Alignment.topLeft,
           child: Column(
@@ -141,14 +115,14 @@ class _DetailWalletState extends State<DetailWallet> {
                     mainAxisAlignment: MainAxisAlignment.start,
                     children: [
                       Text(widget.wallet.tipe, style: kFontStyle(fontSize: 15, color: Colors.white, family: "QuickSand_Medium"),),
-                      Text(widget.wallet.judul, style: kFontStyle(fontSize: 20, color: Colors.white, family: "QuickSand_Medium"),),
+                      Text(widget.wallet.judul, style: kFontStyle(fontSize: 20, color: Colors.white,),)
                     ],
                   ),
                   FutureBuilder(
                       future: widget.wallet.totalUang(),
                       builder: (_, snapshot){
                         if (snapshot.connectionState == ConnectionState.waiting){
-                          return const CircularProgressIndicator();
+                          return Text(formatCurrency(0), style: kFontStyle(fontSize: 25, color: Colors.white),);
                         } else if (snapshot.hasError){
                           return const Text("Sadly, something wrong");
                         } else {
@@ -163,15 +137,6 @@ class _DetailWalletState extends State<DetailWallet> {
       ),
     );
   }
-  KApplicationBar appBar(BuildContext context){
-    return KAppBar(
-      title: "Detail Wallet", 
-      centerTitle: true, 
-      fontColor: Colors.white, 
-      backgroundColor: ApplicationColors.primary,
-      leading: appBarLeading(context)
-    ).getWidget();
-  }
   KWidget         appBarLeading(BuildContext context){
     return GestureDetector(
       onTap: (){
@@ -180,24 +145,87 @@ class _DetailWalletState extends State<DetailWallet> {
       child: const Icon(Icons.arrow_back_ios_new, color: Colors.white,),
     );
   }
-  
+  List<Widget>    appBarAction(BuildContext context){
+    const Color iconColor = Colors.white;
+    const double paddingRight = 10;
+    Widget tombolUbah(){
+      return GestureDetector(
+        onTap: (){},
+        child: const Padding(
+          padding: EdgeInsets.only(right: paddingRight),
+          child: Icon(CupertinoIcons.pencil, color: iconColor,),
+        ),
+      );
+    }
+    Widget tombolHapus(){
+      KEventHandler hapusWallet(){
+        SQLHelperWallet().delete(widget.wallet, db.database).then((value){
+          if (value == 0){
+            tampilkanSnackBar(context, jenisPesan: Pesan.Success, msg: "Wallet berhasil dihapus");
+            widget.callback();
+            Navigator.pop(context);
+          } else {
+            KDialogInfo(
+              title: "Kesalahan", 
+              info: "Terdapat kesalahan saat aplikasi mencoba menghapus data wallet ($value)", 
+              jenisPesan: Pesan.Error
+            ).tampilkanDialog(context);
+          }
+        });
+        
+      }
+      return GestureDetector(
+        onTap: (){
+          KDialogInfo(
+            title: "Anda yakin?", 
+            info: "Semua data yang ada di wallet (pemasukan dan pengeluaran) akan dihapus tidak bisa dikembalikan lagi!", 
+            jenisPesan: Pesan.Konfirmasi,
+            onOk: hapusWallet
+          ).tampilkanDialog(context);
+        },
+        child: const Padding(
+          padding: EdgeInsets.only(right: paddingRight), 
+          child: Icon(CupertinoIcons.delete, color: iconColor,),
+        ),
+      );
+    }
+
+    return [
+      tombolUbah(),
+      tombolHapus(),
+    ];
+  }
+  KApplicationBar appBar(BuildContext context){
+    return KAppBar(
+      title: "Detail Wallet", 
+      centerTitle: true, 
+      fontColor: Colors.white, 
+      backgroundColor: ApplicationColors.primary,
+      leading: appBarLeading(context),
+      action: appBarAction(context),
+    ).getWidget();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+   return Scaffold(
       backgroundColor: ApplicationColors.primary,
       appBar: appBar(context),
       body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            ringkasanWallet(context),
-            listPemasukan(context),
-            dummyHeight(height: 25),
-            listPengeluaran(context),
-            dummyHeight(height: 25),
-            buildDistribusiPengeluaran(context),
-            dummyHeight(height: 25)
-          ],
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              ringkasanWallet(context),
+              listPemasukan(context),
+              dummyHeight(height: 25),
+              listPengeluaran(context),
+              dummyHeight(height: 25),
+              buildDistribusiPengeluaran(context),
+              dummyHeight(height: 25)
+            ],
+          ),
         ),
       ),
     );
