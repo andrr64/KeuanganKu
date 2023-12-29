4f5da687fa8ba3919df1d97d30cb2679ad3fb0eb
andrr64 <derzandrr03@gmail.com>
2023-12-29 15:01:22 +0700

    perapihan kode

--- keuanganku/lib/app/routes/main/wallet/wallet.dart
+++ keuanganku/lib/app/routes/main/wallet/wallet.dart
@@ -5,11 +5,15 @@ import 'package:keuanganku/app/routes/main/pengeluaran/pengeluaran.dart';
 import 'package:keuanganku/app/routes/main/wallet/widgets/k_list_pemasukan/k_list_pemasukan.dart';
 import 'package:keuanganku/app/routes/main/wallet/widgets/k_list_wallet/k_list_wallet.dart';
 import 'package:keuanganku/app/state_bridge.dart';
+import 'package:keuanganku/app/widgets/k_card/k_card.dart';
+import 'package:keuanganku/app/widgets/k_future_builder/k_future.dart';
 import 'package:keuanganku/database/helper/income.dart';
 import 'package:keuanganku/database/helper/wallet.dart';
 import 'package:keuanganku/enum/data_transaksi.dart';
+import 'package:keuanganku/k_typedef.dart';
 import 'package:keuanganku/main.dart';
 import 'package:keuanganku/util/dummy.dart';
+import 'package:keuanganku/util/font_style.dart';
 
 class StateData {
   final Color backgroundColor = Colors.white;
@@ -31,75 +35,62 @@ class _HalamanWalletState extends State<HalamanWallet> {
       
     });
   }
+  void callback(){
+      HalamanBeranda.state.update();
+      HalamanPengeluaran.state.update();
+      HalamanWallet.state.update();
+  }
 
+  KWidget listWallet     () {
+    return KFutureBuilder.build(
+      future:       SQLHelperWallet().readAll(db.database), 
+      whenError:    KCard(
+        title: "Error :X", 
+        child: makeCenterWithRow(child: Text("Sayangnya terdapat kesalahan.", style: kFontStyle(fontSize: 13)))
+      ), 
+      whenWaiting:  KCard(
+        title: "Wait...", 
+        child: makeCenterWithRow(child: const CircularProgressIndicator.adaptive())
+      ),
+      whenSuccess:  (snapshot) => KListWallet(wallets: snapshot, callback: callback)
+    );
+  }
+  KWidget listPemasukan  (){
+    return KFutureBuilder.build(
+      future: SQLHelperIncome().readWeekly(DateTime.now(), db: db.database, sortirBy: SortirTransaksi.Terbaru), 
+      whenError: KCard(
+        title: "Error :X", 
+        child: makeCenterWithRow(child: Text("Sayangnya terdapat kesalahan.", style: kFontStyle(fontSize: 13)))
+      ),
+      whenWaiting: KCard(
+        title: "Wait...", 
+        child: makeCenterWithRow(child: const CircularProgressIndicator.adaptive())
+      ),
+      whenSuccess: (snapshot) => KListPemasukan(listPemasukan: snapshot, callback: callback)
+    );
+  }
+  
   @override
   Widget build(BuildContext context) {
     HalamanWallet.state.init(updateState);
-    Widget listWallet() {
-      void callback(){
-        updateState();
-        HalamanBeranda.state.update();
-        HalamanPengeluaran.state.update();
-        HalamanWallet.state.update();
-      }
-      return FutureBuilder(
-        future: SQLHelperWallet().readAll(db.database), 
-        builder: (_, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return makeCenterWithRow(child: const CircularProgressIndicator());
-          } else if (snapshot.hasError) {
-            return makeCenterWithRow(child: const Text("Something wrong..."));
-          } else {
-            return KListWallet(
-              wallets: snapshot.data!, 
-              callback: (){
-                callback();
-              }
-            );
-          }
-        },
-      );
-    }
-    Widget listPemasukan(){
-      void callback(){
-        HalamanWallet.state.update();
-        HalamanBeranda.state.update();
-        HalamanPengeluaran.state.update();
-      }
-      return FutureBuilder(
-        future: SQLHelperIncome().readWeekly(DateTime.now(), db: db.database, sortirBy: SortirTransaksi.Terbaru), 
-        builder: (_, snapshot){
-          if (snapshot.connectionState == ConnectionState.waiting){
-            return makeCenterWithRow(child: const CircularProgressIndicator());
-          } else if (snapshot.hasError){
-            return makeCenterWithRow(child: const Text("Sadly, something wrong..."));
-          } else {
-            return Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 25),
-              child: KListPemasukan(
-                listPemasukan: snapshot.data!, 
-                callback: callback
-              ),
-            );
-          }
-        }
-      );
-    }
     const double paddingBottom = 15;
 
     return Scaffold(
       backgroundColor: KColors.backgroundPrimary,
       body: SingleChildScrollView(
         scrollDirection: Axis.vertical,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            dummyHeight(height: paddingBottom),
-            listWallet(),
-            dummyHeight(height: paddingBottom),
-            listPemasukan(),
-            dummyHeight(height: paddingBottom),
-          ],
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              dummyHeight(height: paddingBottom),
+              listWallet(),
+              dummyHeight(height: paddingBottom),
+              listPemasukan(),
+              dummyHeight(height: paddingBottom),
+            ],
+          ),
         ),
       )
     );
