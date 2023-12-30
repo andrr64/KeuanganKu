e89107cfb8ac51c9e55049143349fb559d3b1d5a
andrr64 <derzandrr03@gmail.com>
2023-12-30 22:55:21 +0700

    Version 0.1.0 Debug

--- keuanganku/lib/app_info.dart
+++ keuanganku/lib/app_info.dart
@@ -4,8 +4,8 @@ import 'package:keuanganku/main.dart';
 class ApplicationInfo {
   static const String stringAppVersion = "$appMajorVersion.$appMinorVersion.$appPatchVersion";
   static const String title = "KeuanganKu";
-  static const int appMajorVersion = 1;
-  static const int appMinorVersion = 0;
+  static const int appMajorVersion = 0;
+  static const int appMinorVersion = 1;
   static const int appPatchVersion = 0;
   static const BuildMode appBuildMode = BuildMode.Debug;
   static String get buildMode => appBuildMode.enumValue;
